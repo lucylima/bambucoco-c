0c29b4565a36cbbc43d01066791703ac3da1ca30
lucy lima <lucyatmosphere@gmail.com>
2025-11-27 20:08:27 -0300

    fix: menu option fix

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ void bootstrap_restaurante(Mesa r[LINHAS][COLUNAS]);
 void reservar_mesa(Mesa r[LINHAS][COLUNAS]);
 
 int main(void) {
-  FILE *arquivo;
   Mesa restaurante[LINHAS][COLUNAS];
   char op;
 
@@ -52,6 +51,11 @@ int main(void) {
       printf("O que deseja fazer?\n1 - adicionar algo ao cardapio\n2 - remover "
              "algo do cardapio\n3 - ver cardapio\n");
       scanf("%c", &op);
+      switch (op) {
+      case '1':
+        add_cardapio();
+        break;
+      }
 
       break;
 
@@ -62,32 +66,6 @@ int main(void) {
   }
 
   return 0;
-}
-
-void add_cardapio() {
-  ItemCardapio aux_cardapio;
-
-  FILE *arquivo;
-
-  arquivo = fopen("./data/restaurante/cardapio.dat", "ab+");
-
-  if (arquivo == NULL) {
-    printf("erro ao abrir arquivo, abortando\n");
-    exit(1);
-  }
-
-  printf("Digite o ID do item\n>>> ");
-  scanf("%d", &aux_cardapio.id_cardapio);
-
-  printf("Digite o nome do item\n>>> ");
-  fgets(aux_cardapio.nome, sizeof(aux_cardapio.nome), stdin);
-
-  printf("Digite o valor do item\n>>> ");
-  scanf("%f", &aux_cardapio.preco);
-
-  fwrite(&aux_cardapio, sizeof(ItemCardapio), 1, arquivo);
-
-  fclose(arquivo);
 }
 
 void reservar_mesa(Mesa r[LINHAS][COLUNAS]) {
@@ -105,11 +83,5 @@ void bootstrap_restaurante(Mesa r[LINHAS][COLUNAS]) {
       r[i][j].id_mesa = contador++;
       r[i][j].status = 'L';
     }
-  }
-}
-
-void limpar_buffer() {
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF) {
   }
 }
