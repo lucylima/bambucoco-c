dc8c2d4fa9171e76acb4d90fda0c0040844b1c0a
lucy lima <lucyatmosphere@gmail.com>
2025-12-04 01:38:44 -0300

    refactor: book table function

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 
 void limpar_buffer();
 void reservar_mesa(Mesa r[LINHAS][COLUNAS]);
-Mesa buscar_mesa(Mesa r[LINHAS][COLUNAS], int input);
 
 int main(void) {
   Mesa restaurante[LINHAS][COLUNAS];
@@ -17,10 +16,9 @@ int main(void) {
   bootstrap_restaurante(restaurante);
 
   while (1) {
-    system("clear");
     menu();
     printf(">>> ");
-    scanf("%c", &op);
+    scanf(" %c", &op);
 
     switch (op) {
 
@@ -28,7 +26,7 @@ int main(void) {
       limpar_buffer();
       interface_restaurante(restaurante);
       printf("Deseja reservar uma mesa? S/n\n>>> ");
-      scanf("%c", &op);
+      scanf(" %c", &op);
 
       if (op == 's' || op == 'S') {
         limpar_buffer();
@@ -36,7 +34,9 @@ int main(void) {
       }
 
       break;
+
     case '2':
+      limpar_buffer();
       reservar_mesa(restaurante);
       break;
 
@@ -47,6 +47,7 @@ int main(void) {
     case '4':
 
       break;
+
     case '5':
       limpar_buffer();
       printf("O que deseja fazer?\n1 - adicionar algo ao cardapio\n2 - remover "
@@ -63,61 +64,66 @@ int main(void) {
         interface_cardapio();
         break;
       }
-
       break;
 
     case '6':
       exit(0);
       break;
+
+    default:
+      break;
     }
-  default:
-    break;
   }
 
   return 0;
 }
 
 void reservar_mesa(Mesa r[LINHAS][COLUNAS]) {
-  int input = -1;
+  int input = 0;
+  char tam;
+  int encontrou = 0;
 
-  while (1) {
-    interface_restaurante(r);
+  interface_restaurante(r);
 
-    printf("Digite a mesa que deseja reservar\n>>> ");
-    scanf("%d", &input);
-
-    Mesa mesa = buscar_mesa(r, input);
-
-    if (mesa.id_mesa == -1) {
-      printf("Mesa não encontrada! digite novamente\n");
-    }
-
-    if (mesa.status == 'O') {
-      printf("Mesa ocupada! escolha outra\n");
-    }
-
-    if (mesa.status == 'L') {
-      printf("Mesa %d reservada com sucesso\n", mesa.id_mesa);
-      mesa.status = 'O';
-      return;
-    }
-  }
-}
-
-Mesa buscar_mesa(Mesa r[LINHAS][COLUNAS], int input) {
-  Mesa aux;
-
-  aux.status = 'O';
-  aux.id_mesa = -1; // id -1 significa que há algo de errado, a mesa não existe
-                    // ou esta ocupada
+  printf("Digite a mesa que deseja reservar\n>>> ");
+  scanf("%d", &input);
 
   for (int i = 0; i < LINHAS; i++) {
     for (int j = 0; j < COLUNAS; j++) {
-      if (r[i][j].id_mesa == input) {
-        return r[i][j];
+
+      if (input == r[i][j].id_mesa) {
+
+        encontrou = 1;
+
+        if (r[i][j].status == 'O') {
+          printf("mesa ocupada! selecione outra\n");
+          reservar_mesa(r);
+        }
+
+        printf("Digite o tamanho da mesa:\n");
+        printf("P: 10 pedidos\n");
+        printf("M: 15 pedidos\n");
+        printf("G: 20 pedidos\n>>> ");
+        scanf(" %c", &tam);
+
+        switch (tam) {
+        case 'P':
+          r[i][j].comanda = malloc(sizeof(Pedido) * 10);
+          break;
+        case 'M':
+          r[i][j].comanda = malloc(sizeof(Pedido) * 15);
+          break;
+        case 'G':
+          r[i][j].comanda = malloc(sizeof(Pedido) * 20);
+          break;
+        }
+        r[i][j].status = 'O';
+        return;
       }
     }
   }
 
-  return aux;
+  if (!encontrou) {
+    printf("ID da mesa inexistente... selecione outro\n");
+  }
 }
